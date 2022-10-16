368ffa5df0946aa467c9ab0bc5843b47eaf33312
Freddie <lin.pengyu@outlook.com>
2022-10-16 18:34:28 +0800

    first upload

---
+++ all.js
@@ -0,0 +1,146 @@
+const url = "https://hexschool.github.io/js-filter-data/data.json";
+const list = document.querySelector(".showList");
+
+let data = [];
+
+//get data from url
+function getData() {
+  axios.get(url).then(function (response) {
+    data = response.data.filter(
+      (item) => item.作物名稱 != null && item.作物名稱 != "" && item.交易量 != 0
+    );
+    renderData(data);
+  });
+}
+
+getData();
+//render data
+function renderData(arr) {
+  let str = "";
+  arr.forEach((item) => {
+    str += `<tr>
+    <td>${item.作物名稱}</td>
+    <td>${item.市場名稱}</td>
+    <td>${item.上價}</td>
+    <td>${item.中價}</td>
+    <td>${item.下價}</td>
+    <td>${item.平均價}</td>
+    <td>${item.交易量}</td>
+    </tr>`;
+  });
+  list.innerHTML = str;
+}
+
+//filter type
+let typeData = [];
+const buttons = document.querySelector(".button-group");
+buttons.addEventListener("click", (e) => {
+  if (e.target.nodeName == "BUTTON") {
+    let tabs = document.querySelectorAll(".button-group button");
+    tabs.forEach((item) => {
+      item.classList.remove("active");
+    });
+    let type = e.target.dataset.type;
+    if (type == "N04") {
+      typeData = data.filter((item) => item.種類代碼 == "N04");
+    } else if (type == "N05") {
+      typeData = data.filter((item) => item.種類代碼 == "N05");
+    } else if (type == "N06") {
+      typeData = data.filter((item) => item.種類代碼 == "N06");
+    } else {
+      typeData = data;
+    }
+    e.target.classList.add("active");
+    renderData(typeData);
+    reset();
+  }
+});
+
+//search bar
+const search = document.querySelector(".search-group");
+const crop = document.querySelector("#crop");
+search.addEventListener("click", (e) => {
+  if (e.target.nodeName == "BUTTON") {
+    if (crop.value.trim() == "") {
+      return;
+    }
+    searchFor();
+  }
+});
+
+crop.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    searchFor();
+  }
+});
+
+function searchFor() {
+  let filterData = [];
+  filterData = typeData.filter((item) => {
+    return item.作物名稱.toLowerCase().match(crop.value.toLowerCase().trim());
+  });
+  if (filterData.length == 0) {
+    list.innerHTML = `<tr><td colspan="6" class="text-center p-3">查詢不到交易資訊QQ</td></tr>`;
+  } else {
+    renderData(filterData);
+  }
+  reset();
+  crop.value = "";
+}
+
+//sort select
+const sort = document.querySelector(".sort-select");
+sort.addEventListener("change", (e) => {
+  switch (e.target.value) {
+    case "依上價排序":
+      selectChange("上價");
+      break;
+    case "依中價排序":
+      selectChange("中價");
+      break;
+    case "依下價排序":
+      selectChange("下價");
+      break;
+    case "依平均價排序":
+      selectChange("平均價");
+      break;
+    case "依交易量排序":
+      selectChange("交易量");
+      break;
+    default:
+  }
+});
+
+function selectChange(value) {
+  typeData.sort((a, b) => {
+    return a[value] - b[value];
+  });
+  renderData(typeData);
+}
+
+//sort by arrows
+const sortAdv = document.querySelector(".js-sort-advanced");
+sortAdv.addEventListener("click", (e) => {
+  if (e.target.nodeName == "I") {
+    let sortPrice = e.target.dataset.price;
+    let sortArrow = e.target.dataset.sort;
+    if (sortArrow == "up") {
+      typeData.sort((a, b) => {
+        return b[sortPrice] - a[sortPrice];
+      });
+    } else {
+      typeData.sort((a, b) => {
+        return a[sortPrice] - b[sortPrice];
+      });
+    }
+    reset();
+    renderData(typeData);
+  }
+});
+
+//reset sort value to default
+function reset() {
+  if (sort.value != "排序篩選") {
+    sort.value = "排序篩選";
+  }
+}
